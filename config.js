09e991ca249ed5c1a98b30480bc0d85a85e7abb4
Simen Haugerud Granlund <simen@hgranlund.com>
2017-04-09 22:17:34 +0200

    Added initial config to cheslie-board og chelie-lobby

--- config.js
+++ config.js
@@ -2,17 +2,51 @@ var env = process.env.NODE_ENV || 'development';
 
 var config = {
   development: {
-    app: {
-      name: 'cheslie-game'
+    game: {
+      url: 'http://localhost',
+      app: {
+        name: 'cheslie-game'
+      },
+      port: 2204,
     },
-    port: process.env.PORT || 3000,
+    lobby: {
+      url: 'http://localhost',
+      app: {
+        name: 'cheslie-lobby'
+      },
+      port: 2205,
+    },
+    board: {
+      url: 'http://localhost',
+      app: {
+        name: 'cheslie-board'
+      },
+      port: 2206,
+    }
   },
 
   production: {
-    app: {
-      name: 'cheslie-game'
+    game: {
+      url: 'http://cheslie-game.azurewebsites.net',
+      app: {
+        name: 'cheslie-game'
+      },
+      port: process.env.PORT,
     },
-    port: process.env.PORT,
+    lobby: {
+      url: 'http://cheslie-lobby.azurewebsites.net',
+      app: {
+        name: 'cheslie-lobby'
+      },
+      port: process.env.PORT,
+    },
+    board: {
+      url: 'http://cheslie-board.herokuapp.com',
+      app: {
+        name: 'cheslie-board'
+      },
+      port: process.env.PORT,
+    }
   }
 };
 
